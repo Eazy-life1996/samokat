4e862878ce8685a094ced03ce680767093e051a3
Yurii <eazylifemusic@gmail.com>
2023-02-12 16:24:50 +0600

    Сделал два блока надододелать дкеор

--- js/main.js
+++ js/main.js
@@ -10,4 +10,13 @@ window.addEventListener('DOMContentLoaded', () => {
   closeBtn.addEventListener('click', () => {
     menu.classList.add('header__nav-close');
   });
+
+  // Открытие списка с карточке
+
+  const list = document.querySelector('[data-list]'),
+        buttonForList = document.querySelector('[data-show-list]');
+
+  buttonForList.addEventListener('click', () => {
+    list.classList.toggle('card__list--visible');
+  })
 });
